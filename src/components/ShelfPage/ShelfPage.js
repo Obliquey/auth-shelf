6039bba5b879f17a74637088845a384f597bdbe8
Anders Boyum <Acboyum@gmail.com>
2023-05-18 13:49:37 -0500

    Shelf page dispatch setup, fetch_things saga setup, GET route in shelf.router setup

--- src/components/ShelfPage/ShelfPage.js
+++ src/components/ShelfPage/ShelfPage.js
@@ -1,6 +1,20 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
 
 function ShelfPage() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    fetchThings();
+  }, [])
+  
+  const fetchThings = () => {
+    dispatch({
+      type: 'SAGA/FETCH_THINGS'
+    })
+  }
+
   return (
     <div className="container">
       <h2>Shelf</h2>
---
+++ src/redux/sagas/Fetch_things.js
@@ -0,0 +1,16 @@
+import { put, takeLatest } from 'redux-saga/effects';
+import axios from 'axios';
+
+function* fetchThings() {
+    try{
+        const things = yield axios.get('/api/shelf')
+        console.log("here are our things:", things.data);
+        
+    } catch{
+        console.log("Error connecting to server, couldn't get our things");
+    }
+}
+function* sagaFetchThings() {
+    yield takeLatest('SAGA/FETCH_THINGS', fetchThings)
+}
+export default sagaFetchThings;
